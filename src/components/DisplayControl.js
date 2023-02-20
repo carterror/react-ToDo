addbc98a2e8d148284175966a3b8de6cd73b2d69
carterror <carlos.bramila98@gmail.com>
2023-02-19 23:19:30 -0500

    finaly

--- src/components/DisplayControl.js
+++ src/components/DisplayControl.js
@@ -2,15 +2,23 @@ export const DisplayControl = ({isCheked, setshowDone, cleanTask}) => {
 
 
   return (
-    <div>
-      <input
-        type="checkbox"
-        onChange={(e) => setshowDone(e.target.checked)}
-        checked={isCheked}
-      />
-      Show Task Completed
-
-      <button className="btn btn-primary" onClick={() => cleanTask()}>Clear</button>
+    <div className="d-flex justify-content-between bg-secondary text-white text-center p-2 border-secondary">
+      <div className="form-check form-switch py-2" >
+        <input
+          id="defaultCheck1"
+          className="form-check-input"
+          type="checkbox"
+          onChange={(e) => setshowDone(e.target.checked)}
+          checked={isCheked}
+        />
+        <label className="form-check-label " for="defaultCheck1">
+          Show Task Completed
+        </label>
+      </div>
+      
+      <button className="btn btn-danger" onClick={() => cleanTask()}>
+        Clear
+      </button>
     </div>
   );
 };
